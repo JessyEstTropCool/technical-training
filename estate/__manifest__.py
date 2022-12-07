fda82ae5b0a18119e92333afe5c5321f85b03099
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-12-07 11:12:39 +0100

    Estate start

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0",  # Version
-    "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies
-    "data": [
-
-    ],
-    "installable": True,
-    'license': 'LGPL-3',
+    'name': "estate",
+    'summary': """Project for HELB Student""",
+    'author': "Jessy Adam",
+    'website': "http://jadam.alwaysdata.net",
+    'category': 'Uncategorized',
+    'version': '16.0,
+    'depends': ['base', 'sale_management', 'calendar', 'hr'],
+    'data': [
+    ]
 }
