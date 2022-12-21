e4a6b5797f1a60c043d8b4419336687476f239d1
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-12-21 17:03:41 +0100

    this is it

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -1,5 +1,23 @@
+from datetime import timedelta
 from odoo import api, models, _, fields
 from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+
+        for line in self.order_line:
+            if line.training_date and line.selected_employee:
+                self.env['calendar.event'].create({
+                    'name':'Training',
+                    'start_date':line.training_date,
+                    'stop_date':line.training_date + timedelta(hours=8),
+                    'allday':True,
+                    'partner_ids':[line.selected_employee.user_partner_id]
+                })
+
+        return res
+
+
