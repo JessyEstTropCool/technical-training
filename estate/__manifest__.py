5552b5e884d1276e7caf77f9a56e41481ba29801
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-12-21 14:19:19 +0100

    sober license

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "http://jadam.alwaysdata.net",
     'category': 'Uncategorized',
     'version': '16.0',
-    'liescence': 'LGPL-3',
+    'license': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': ["views/res_groups.xml","views/res_partner.xml","views/sale_order.xml"]
 }
