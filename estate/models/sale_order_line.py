f5f0269bed1b698af792c66c2b56c1d422d48293
Jessy Adam <80044343+JessyEstTropCool@users.noreply.github.com>
2022-12-21 15:50:30 +0100

    da empz

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,3 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
+    selected_employee = fields.Many2one(comodel_name="hr.employee", string="Selected Employee", ondelete="set null")
