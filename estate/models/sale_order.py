13271644946ecc02c941b7a36b28b364c85f97d5
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2023-01-04 15:41:03 +0100

    no partner 2

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -17,13 +17,12 @@ class SaleOrder(models.Model):
                         'start_date':line.training_date,
                         'stop_date':line.training_date + timedelta(hours=8),
                         'allday':True,
-                        'partner_ids':[(4, partner.id), (4, line.selected_employee.partner_id.id)],
+                        'partner_ids':[(4, partner.id), (4, line.selected_employee.user_id.partner_id.id)],
                     })
                 else:
                     raise ValueError(f"""THERE IS NO PARTNER !!!!!!!!!!!!!!
                     \nemployee.user_partner_id = {line.selected_employee.user_partner_id}
                     \nemployee.user_id = {line.selected_employee.user_id}
-                    \nemployee.partner_id = {line.selected_employee.partner_id}
                     \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id}""")
 
         return res
