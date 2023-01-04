f89de1b296d178fec89180969cc5e5157eb36222
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2023-01-04 15:37:29 +0100

    there is no partner

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -11,14 +11,20 @@ class SaleOrder(models.Model):
 
         for line in self.order_line:
             if line.training_date and line.selected_employee:
-                #raise TimeoutError(line.selected_employee.user_partner_id)
-                self.env['calendar.event'].create({
-                    'name':'Training',
-                    'start_date':line.training_date,
-                    'stop_date':line.training_date + timedelta(hours=8),
-                    'allday':True,
-                    'partner_ids':[(4, partner.id), (4, line.selected_employee.user_id.partner_id.id)],
-                })
+                if line.selected_employee.partner_id:
+                    self.env['calendar.event'].create({
+                        'name':'Training',
+                        'start_date':line.training_date,
+                        'stop_date':line.training_date + timedelta(hours=8),
+                        'allday':True,
+                        'partner_ids':[(4, partner.id), (4, line.selected_employee.partner_id.id)],
+                    })
+                else:
+                    raise ValueError(f"""THERE IS NO PARTNER !!!!!!!!!!!!!!
+                    \nemployee.user_partner_id = {line.selected_employee.user_partner_id}
+                    \nemployee.user_id = {line.selected_employee.user_id}
+                    \nemployee.partner_id = {line.selected_employee.partner_id}
+                    \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id}""")
 
         return res
 
