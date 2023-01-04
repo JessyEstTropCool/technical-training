5a540f4e051a4a0d50fc2fddbbd06eca2d72028b
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2023-01-04 16:02:59 +0100

    Did I do this right

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -7,23 +7,34 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
-        partner = self.partner_id
 
         for line in self.order_line:
             if line.training_date and line.selected_employee:
+                line_partner = None
                 if line.selected_employee.user_id:
-                    self.env['calendar.event'].create({
-                        'name':'Training',
-                        'start_date':line.training_date,
-                        'stop_date':line.training_date + timedelta(hours=8),
-                        'allday':True,
-                        'partner_ids':[(4, partner.id), (4, line.selected_employee.user_id.partner_id.id)],
-                    })
+                    line_partner = line.selected_employee.user_partner_id
                 else:
-                    raise ValueError(f"""THERE IS NO PARTNER !!!!!!!!!!!!!!
-                    \nemployee.user_partner_id = {line.selected_employee.user_partner_id}
-                    \nemployee.user_id = {line.selected_employee.user_id}
-                    \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id}""")
+                    presumed_partner = models.env['res.partner'].search([('name', '=', line.selected_employee.name)], limit=1)
+                    if len(presumed_partner) == 0:
+                        line.selected_employee.user_partner_id = self.env['res.partner'].create({
+                            'name': line.selected_employee.name,
+                        })
+                        line_partner = line.selected_employee.user_partner_id
+                    else:
+                        line_partner = presumed_partner
+
+                raise ValueError(f"""THERE IS NO PARTNER !!!!!!!!!!!!!!
+                \nemployee.user_partner_id = {line.selected_employee.user_partner_id}
+                \nemployee.user_id = {line.selected_employee.user_id}
+                \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id}""")
+                
+                self.env['calendar.event'].create({
+                    'name':'Training',
+                    'start_date':line.training_date,
+                    'stop_date':line.training_date + timedelta(hours=8),
+                    'allday':True,
+                    'partner_ids':[(4, line_partner.id)],
+                })
 
         return res
 
