121658bf5b0cdb188a69a1c629ee9fd33bff3ee7
JessyEstTropCool <80044343+JessyEstTropCool@users.noreply.github.com>
2023-01-04 22:41:41 +0100

    Revert "Making activity"
    This reverts commit 9ae951415d15bb0e8864e3d5d1e46d1fd86da163.

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -12,10 +12,10 @@ class SaleOrder(models.Model):
             for line in self.order_line:
                 if line.training_date and line.selected_employee:
                     line_partner = None
-                    #path = "None"
+                    path = "None"
                     if line.selected_employee.user_id:
                         line_partner = line.selected_employee.user_partner_id
-                        #path = "has user"
+                        path = "has user"
                     else:
                         presumed_partner = self.env['res.partner'].search([('name', '=', line.selected_employee.name)], limit=1)
                         if len(presumed_partner) == 0:
@@ -23,19 +23,19 @@ class SaleOrder(models.Model):
                                 'name': line.selected_employee.name,
                             })
                             line_partner = line.selected_employee.user_partner_id
-                            #path = "new user"
+                            path = "new user"
                         else:
                             line.selected_employee.user_partner_id = presumed_partner
                             line_partner = presumed_partner
-                            #path = "searched user"
+                            path = "searched user"
 
-                    # self.message_post(body=f"""This is where the partner comes from
-                    #     \nemployee.user_partner_id = {line.selected_employee.user_partner_id.name}
-                    #     \nemployee.user_id = {line.selected_employee.user_id.name}
-                    #     \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id.name}
-                    #     \nline_partner = {line_partner.name}
-                    #     \npath = {path}
-                    #     \nmaxamount = {max_amount_approvable}""")
+                    self.message_post(body=f"""This is where the partner comes from
+                        \nemployee.user_partner_id = {line.selected_employee.user_partner_id.name}
+                        \nemployee.user_id = {line.selected_employee.user_id.name}
+                        \nemployee.user_id.partner_id = {line.selected_employee.user_id.partner_id.name}
+                        \nline_partner = {line_partner.name}
+                        \npath = {path}
+                        \nmaxamount = {max_amount_approvable}""")
                     
                     self.env['calendar.event'].create({
                         'name':'Training',
@@ -53,37 +53,29 @@ class SaleOrder(models.Model):
     def get_user_max_amount(self):
         user = self.env.user
         if user.partner_id.max_amount != 0:
-            #self.message_post(body=f"Max amount (user) = {user.partner_id.max_amount}")
+            self.message_post(body=f"Max amount (user) = {user.partner_id.max_amount}")
             return user.partner_id.max_amount
 
         max = 500
-        #cool_str = "The groups"
+        cool_str = "The groups"
+        help(user)
 
         for group in user.groups_id:
-            # cool_str += "\n" + group.name + ", ma = "
-            # if group.max_amount:
-            #     cool_str += str(group.max_amount)
+            cool_str += "\n" + group.name + ", ma = "
+            if group.max_amount:
+                cool_str += str(group.max_amount)
 
             if group.max_amount and group.max_amount > max:
                 max = group.max_amount
 
-        # self.message_post(body=f"""
-        #     Max amount (groups) = {max},
-        #     g:{cool_str}
-        #     self: {self.name} {self}
-        #     self.user_id: {self.user_id.name} {self.user_id}
-        #     groups:{self.user_id.groups_id}""")
+        self.message_post(body=f"""
+            Max amount (groups) = {max},
+            g:{cool_str}
+            self: {self.name} {self}
+            self.user_id: {self.user_id.name} {self.user_id}
+            groups:{self.user_id.groups_id}""")
 
         return max
 
-    def ask_approval(self):
-        for line in self.order_line:
-            if line.training_date and line.selected_employee:
-                self.activity_schedule(
-                    body='Need approval for quotation',
-                    activity_type='approval',
-                    date_deadline=line.training_date,
-                    note=f'Total amount of the sale : {self.self.amount_total}',
-                    auto_remove=True
-                )
+    def trigger_error(self):
         raise TimeoutError("You're in")
